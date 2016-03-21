bc63766051b505d04e17efef7b0fed5b5cfecdda
Roberto Cortez <radcortez@yahoo.com>
2016-03-20 18:39:32 -0700

    Added clean to metamodel generation.

--- src/main/groovy/com/radcortez/gradle/plugin/openjpa/OpenJpaPlugin.groovy
+++ src/main/groovy/com/radcortez/gradle/plugin/openjpa/OpenJpaPlugin.groovy
@@ -6,6 +6,8 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginConvention
 
+import java.nio.file.Paths
+
 /**
  * Description.
  *
@@ -29,13 +31,34 @@ class OpenJpaPlugin implements Plugin<Project> {
                 description: "Generates metamodel classes.",
                 "metamodel")
 
-        project.tasks.classes.doLast { project.tasks.enhance.execute() }
-
         project.afterEvaluate() {
-            if (project.extensions.findByType(OpenJpaExtension).metamodel) {
+            def OpenJpaExtension configuration = project.extensions.openjpa
+
+            project.tasks.clean.doLast { cleanMetamodelOutputFolder(project) }
+
+            project.tasks.classes.doLast { project.tasks.enhance.execute() }
+
+            if (configuration.metamodel) {
                 project.convention.getPlugin(JavaPluginConvention).sourceSets.all {
                     project.tasks.compileJava.dependsOn project.tasks.metamodel
                 }
+            }
+        }
+    }
+
+    private static void cleanMetamodelOutputFolder(Project project) {
+        def OpenJpaExtension configuration = project.extensions.openjpa
+
+        def metamodelOutputFolder = project.file(configuration.metamodelOutputFolder)
+        def path = Paths.get(metamodelOutputFolder.path)
+        metamodelOutputFolder.deleteDir()
+
+        while (path.parent != Paths.get(project.buildDir.path)) {
+            path = path.parent
+            if (path.toFile().listFiles().length == 0) {
+                path.deleteDir()
+            } else {
+                break;
             }
         }
     }
